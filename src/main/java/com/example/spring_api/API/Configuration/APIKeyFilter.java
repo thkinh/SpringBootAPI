612c1263150fd1e51a7f457bb058c6a6d22c805e
thkinh <thkinh2008@gmail.com>
2024-11-29 23:54:38 +0700

    Added APIKEy

---
+++ src/main/java/com/example/spring_api/API/Configuration/APIKeyFilter.java
@@ -0,0 +1,27 @@
+package com.example.spring_api.API.Configuration;
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class APIKeyFilter extends OncePerRequestFilter {
+
+    private static final String API_KEY = "thinhdeptrai1234";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String apiKey = request.getHeader("X-API-KEY");
+        if (API_KEY.equals(apiKey)) {
+            filterChain.doFilter(request, response);
+        } else {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.getWriter().write("Forbidden");
+        }
+    }
+}
---
+++ src/main/java/com/example/spring_api/API/Configuration/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.spring_api.API.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(registry -> {
+                registry.anyRequest().permitAll(); // Require authentication for all requests
+            })
+            .addFilterBefore(new APIKeyFilter(), UsernamePasswordAuthenticationFilter.class) // Add the custom filter
+            .build();
+    }
+}
+
